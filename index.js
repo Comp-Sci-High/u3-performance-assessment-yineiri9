29370bd7c9a438710e50d74081fb2d3dc6ca1387
yineiri9 <yineiri.reyescepeda25@compscihigh.org>
2025-01-06 15:28:44 +0000

    Started performace assesment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Good luck!
+const express = require('express');
+const { request } = require('http');
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server running")
+})
